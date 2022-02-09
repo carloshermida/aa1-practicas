f342429541d70a8bfed68a8e1cc176a4b51dbba7
Carlos Hermida <carloshermay@gmail.com>
2022-02-09 20:11:30 +0100

    main v1.0.0

---
+++ main.jl
@@ -0,0 +1,41 @@
+
+function cat_to_num(dataset, cat_indexes)
+    dataset = convert(Array{Any},dataset);
+    for cat_index in cat_indexes
+        categorical = dataset[:,cat_index];
+        cat_options = unique(categorical, dims = 1);
+
+        if length(cat_options) == 2
+            let normal = "0";
+                for i = 1:2
+                    dataset[:,cat_index] = replace(dataset[:,cat_index],
+                     cat_options[i,1] => normal);
+                    normal = "1";
+                end
+            end
+        else
+            let normal = string.(repeat("0",length(cat_options)-1), "1");
+                for i = 1:length(cat_options)
+                    dataset[:,cat_index] = replace(dataset[:,cat_index],
+                     cat_options[i,1] => normal);
+                    normal = string.(normal[2:end], "0");
+                end
+            end
+        end
+    end
+    return dataset;
+end
+
+#######################################
+### INDICA EL DATASET Y LOS INDICES ###
+#######################################
+using DelimitedFiles
+
+dataset_iris = readdlm("iris.data",',');
+cat_indexes_iris = 5;
+dataset_iris = cat_to_num(dataset_iris, cat_indexes_iris);
+
+dataset_haber = readdlm("haberman.data",',');
+cat_indexes_haber = 4;
+dataset_haber = cat_to_num(dataset_haber, cat_indexes_haber);
+#######################################
