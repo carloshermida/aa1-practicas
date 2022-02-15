a4be49ec51ad4d70b7d14f6ebefc1dfdbf48b6dc
Carlos Hermida <carloshermay@gmail.com>
2022-02-15 16:42:58 +0100

    version ordenada

--- main.jl
+++ main.jl
@@ -2,15 +2,20 @@
 #               PRÁCTICA APRENDIZAJE AUTOMÁTICO I / v1.2
 #         Nina López | Borja Souto | Carmen Lozano | Carlos Hermida
 
+
+using Statistics
+using DelimitedFiles
+
 ############################## FUNCIONES ##############################
 
+##### oneHotEncoding
+
+# Función principal
 function oneHotEncoding(feature::AbstractArray{<:Any,1}, classes)
-    """Función para pasar de variable categorica a numérica binaria"""
     if length(classes) == 2
         feature=feature.==classes[1]
         reshape(feature, (length(feature),1))
         return feature;
-
     else length(classes) > 2
         bool_matrix=falses(length(feature),length(classes))
         for i=1:length(classes)
@@ -33,108 +38,166 @@ function oneHotEncoding(feature::AbstractArray{Bool,1})
     end
 end
 
-############################### CÓDIGO ###############################
 
-##### Pasar a binario las categóricas
+##### Máximos y mínimos
 
-using DelimitedFiles
-dataset_iris = readdlm("iris.data",',');
-dataset_iris = permutedims(dataset_iris)
-classes_iris = unique(dataset_iris[5,:])
-feature_iris = dataset_iris[5,:]
-feature_iris = convert(Array{Any},feature_iris);
-feature_iris2 = convert(AbstractArray{<:Any,1}, feature_iris)
-d = oneHotEncoding(feature_iris2, classes_iris)
-
-dataset_haber = readdlm("haberman.data",',');
-dataset_haber = dataset_haber'
-classes_haber = unique(dataset_haber[4,:])
-feature_haber = dataset_haber[4,:]
-feature_haber1 = convert(AbstractArray{<:Any,1},feature_haber);
-d = oneHotEncoding(feature_haber1, classes_haber)
-m = oneHotEncoding(d)
-numerics_haber=dataset_haber[1:3,:]'
-
-##### Normalización
-
-# obtener max y min
+# Función auxiliar
 function calculateMinMaxNormalizationParameters(numerics::AbstractArray{<:Real,2})
     m= maximum(numerics, dims=1)
     mi=minimum(numerics, dims=1)
     return (m, mi)
 end
-calculateMinMaxNormalizationParameters(numerics_haber)
+
+# Función sobrecargada 1
+function normalizeMinMax!(numerics::AbstractArray{<:Real,2}, MinMax::NTuple{2, AbstractArray{<:Real,2}})
+    return (numerics.-MinMax[2])./(MinMax[1].-MinMax[2])
+end
+
+# Función sobrecargada 2
+normalizeMinMax!(numerics::AbstractArray{<:Real,2})=(normalizeMinMax!(numerics, calculateMinMaxNormalizationParameters(numerics)))
+
+# Función sobrecargada 3
+function normalizeMinMax(numerics::AbstractArray{<:Real,2}, MinMax::NTuple{2, AbstractArray{<:Real,2}})
+    copia=copy(numerics)
+    return (copia.-MinMax[2])./(MinMax[1].-MinMax[2])
+end
+
+# Función sobrecargada 4
+normalizeMinMax(numerics::AbstractArray{<:Real,2})=(copia=copy(numerics);normalizeMinMax(copia, calculateMinMaxNormalizationParameters(numerics)))
 
 
-# obtener media sd
-using Statistics
+##### Media y desviación típica
+
+# Función principal
 function calculateZeroMeanNormalizationParameters(numerics::AbstractArray{<:Real,2})
     media=mean(numerics, dims=1)
     sd= std(numerics, dims=1)
     return (media, sd)
 end
 
-calculateZeroMeanNormalizationParameters(numerics_haber)
-
-# normalizaciion max min 1
-function normalizeMinMax!(numerics::AbstractArray{<:Real,2}, MinMax::NTuple{2, AbstractArray{<:Real,2}})
-    return (numerics.-MinMax[2])./(MinMax[1].-MinMax[2])
-end
-
-# normalizacion max min 2
-normalizeMinMax!(numerics::AbstractArray{<:Real,2})=(normalizeMinMax!(numerics, calculateMinMaxNormalizationParameters(numerics)))
-normalizeMinMax!(numerics_haber)
-
-# normalizacion max min 3
-function normalizeMinMax(numerics::AbstractArray{<:Real,2}, MinMax::NTuple{2, AbstractArray{<:Real,2}})
-    copia=copy(numerics)
-    return (copia.-MinMax[2])./(MinMax[1].-MinMax[2])
-end
-
-# normalizacion max min 4
-normalizeMinMax(numerics_haber, calculateMinMaxNormalizationParameters(numerics_haber))
-
-normalizeMinMax(numerics::AbstractArray{<:Real,2})=(copia=copy(numerics);normalizeMinMax(copia, calculateMinMaxNormalizationParameters(numerics)))
-normalizeMinMax(numerics_haber)
-
-# normalizacion media sd 1
+# Función sobrecargada 1
 function normalizeZeroMean!(numerics::AbstractArray{<:Real,2}, media_sd::NTuple{2, AbstractArray{<:Real,2}})
     return (numerics.-media_sd[1])./media_sd[2]
 end
 
-# normalizacion media sd 2
-normalizeZeroMean!(numerics_haber,calculateZeroMeanNormalizationParameters(numerics_haber))
-
+# Función sobrecargada 2
 normalizeZeroMean!(numerics::AbstractArray{<:Real,2})=(normalizeZeroMean!(numerics,calculateZeroMeanNormalizationParameters(numerics_haber)))
-normalizeZeroMean!(numerics_haber)
 
-# normalizacion media sd 3
+# Función sobrecargada 3
 function normalizeZeroMean(numerics::AbstractArray{<:Real,2}, media_sd::NTuple{2, AbstractArray{<:Real,2}})
     copia=copy(numerics)
     return (copia.-media_sd[1])./media_sd[2]
 end
 
-# normalizacion media sd 4
+# Función sobrecargada 4
 normalizeZeroMean(numerics::AbstractArray{<:Real,2})=(copia=copy(numerics);normalizeZeroMean(copia,calculateZeroMeanNormalizationParameters(numerics)))
 
-normalizeZeroMean(numerics_haber)
+
+##### classifyOutputs
+
+# Función principal
+function classifyOutputs(outputs::AbstractArray{<:Real,2}, threshold = 0.5)
+    columns = size(outputs)[2]
+    if columns == 1
+        outputs .>= threshold
+    else
+        (_,indicesMaxEachInstance) = findmax(outputs, dims=2);
+        outputs = falses(size(outputs));
+        outputs[indicesMaxEachInstance] .= true;
+    end
+    return outputs
+end
 
 
+##### accuracy
 
-# Normalizar por columnas de la primera forma:
-norm = (d.-media)./sd
+# Función sobrecargada 1
+function accuracy(targets::AbstractArray{Bool,1}, outputs::AbstractArray{Bool,1})
+    classComparison = targets .== outputs
+    return sum(classComparison)/length(classComparison)
+end
+
+# Función sobrecargada 2
+function accuracy(targets::AbstractArray{Bool,2}, outputs::AbstractArray{Bool,2})
+    rows = size(outputs)[1]
+    columns = size(outputs)[2]
+    if columns == 1
+        targets = (targets[:,1])
+        outputs = (outputs[:,1])
+        accuracy(targets, outputs)
+    else
+        classComparison = falses(rows,1)
+        for row = 1:rows
+            classComparison[row,1] = targets[row,:] == outputs[row,:]
+        end
+        return classComparison
+    end
+end
+
+##
+############################### CÓDIGO ###############################
+
+##### oneHotEncoding
+
+### IRIS
+dataset_iris = readdlm("iris.data",',');
+dataset_iris = permutedims(dataset_iris)
+classes_iris = unique(dataset_iris[5,:])
+feature_iris = dataset_iris[5,:]
+feature_iris = convert(AbstractArray{<:Any,1}, feature_iris)
+
+feature_iris_1 = oneHotEncoding(feature_iris, classes_iris)    # Función principal
+feature_iris_2 = oneHotEncoding(feature_iris)                  # Función sobrecargada 1
+
+### HABERMAN
+dataset_haber = readdlm("haberman.data",',');
+dataset_haber = dataset_haber'
+classes_haber = unique(dataset_haber[4,:])
+feature_haber = dataset_haber[4,:]
+feature_haber = convert(AbstractArray{<:Any,1},feature_haber);
+
+feature_haber_1 = oneHotEncoding(feature_haber, classes_haber)  # Función principal
+feature_haber_2 = oneHotEncoding(feature_haber)                 # Función sobrecargada 1
+feature_haber_3 = oneHotEncoding(feature_haber_2)               # Función sobrecargada 2
+
+##### Máximos y mínimos
+numerics_haber=dataset_haber[1:3,:]'
+normalizeMinMax!(numerics_haber, calculateMinMaxNormalizationParameters(numerics_haber))    # Función sobrecargada 1
+normalizeMinMax!(numerics_haber)                                                            # Función sobrecargada 2
+normalizeMinMax(numerics_haber, calculateMinMaxNormalizationParameters(numerics_haber))     # Función sobrecargada 3
+normalizeMinMax(numerics_haber)                                                             # Función sobrecargada 4
+
+##### Media y desviación típica
+normalizeZeroMean!(numerics_haber,calculateZeroMeanNormalizationParameters(numerics_haber)) # Función sobrecargada 1
+normalizeZeroMean!(numerics_haber)                                                          # Función sobrecargada 2
+normalizeZeroMean(numerics_haber,calculateZeroMeanNormalizationParameters(numerics_haber))  # Función sobrecargada 3
+normalizeZeroMean(numerics_haber)                                                           # Función sobrecargada 4
+
+##### Normalizar por columnas
+d = dataset_haber[:,1:3]
+m = maximum(d, dims=1)
+mi = minimum(d, dims=1)
+med = mean(d, dims=1)
+sd = std(d, dims=1)
+
+### Forma 1:
+norm = (d.-med)./sd
 for i=1:size(d,2)
     if sd[i]==0
         norm[:,i].=0
     end
 end
-norm
 
-# Normalizar por columnas de la segunda forma:
+### Forma 2:
 norm_2 = (d.-mi)./(m.-mi)
 for i=1:size(d,2)
     if mi[i]==m[i]
         norm_2[:,i].=0
     end
 end
-norm_2
+
+##### Test accuracy functions
+
+targets = falses(4,2)
+outputs = falses(4,2)
+accuracy(targets, outputs)
